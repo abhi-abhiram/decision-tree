6933a79a9368fdef33b231f6bce7028460cd3105
Abhiram <anukoolaabhiram@outlook.com>
2023-01-29 21:55:13 +0530

    added connection string as default config for db

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -3,14 +3,11 @@ import { Logic } from '@/graphql/schema/logic.schema';
 import { FormField, SearchOption } from '@/graphql/schema/field.schema';
 import { Form } from '@/graphql/schema/form.schema';
 import { Workspace } from '@/graphql/schema/workspace.schema';
+import { env } from '@/env/server.mjs';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 3306,
-  username: 'test',
-  password: 'test',
-  database: 'test',
+  url: env.DB_URL,
   entities: [FormField, SearchOption, Form, Workspace, Logic],
   synchronize: true,
 });
